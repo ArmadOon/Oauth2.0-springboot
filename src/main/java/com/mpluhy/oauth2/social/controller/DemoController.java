a2c830acb4be167da1c39a8f507edca3948d961f
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-01-10 21:20:24 +0100

    Init, config file for Oauth and demo controller

---
+++ src/main/java/com/mpluhy/oauth2/social/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.mpluhy.oauth2.social.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
